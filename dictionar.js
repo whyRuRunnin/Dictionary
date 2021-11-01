32d70a0d451148dec4c4c3f43bb602cd0f24b83a
whyRuRunnin <popescu.robert.schmidt@gmail.com>
2021-11-01 03:28:34 +0100

    Dictionar 1.0

---
+++ dictionar.js
@@ -0,0 +1,24 @@
+let dictionary = [];
+document.getElementById("add").onclick = function() {
+    var text = document.getElementById("input").value;
+    var li = "<li>" + text + "</li>";
+    document.getElementById("list").innerHTML += li;
+    document.getElementById("input").value = "";
+    let newLength = dictionary.push(text);
+
+}
+document.getElementById("show").onclick = function() {
+    let cond = 0;
+    dictionary.forEach(function(item, index, array) {
+        var x = document.getElementById("input").value;
+        if (x === item) {
+            cond = 1;
+        }
+    })
+    if (cond === 1) {
+        console.log("The word: ", document.getElementById("input").value, " exists in the dictionary");
+    } else {
+        console.log("The word: ", document.getElementById("input").value, " does not exist in the dictionary");
+    }
+    document.getElementById("input").value = "";
+}
